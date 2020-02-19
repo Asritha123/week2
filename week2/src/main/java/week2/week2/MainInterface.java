2fedb9feb2b6c7ec69076a9e2077e22964245aef
DELL <DELL@USER>
2020-02-19 14:47:33 +0530

    week2

---
+++ week2/src/main/java/week2/week2/MainInterface.java
@@ -0,0 +1,6 @@
+package week2.week2;
+
+public interface MainInterface {
+	public int chocolate_sweets(int chocolates[],int sweets[],int total_weight);
+	public int no_of_candy(int low,int high,int x);
+}
